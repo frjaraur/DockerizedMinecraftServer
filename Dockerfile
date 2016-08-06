91853fd608f96eb6b79c3bb819750ba09d103673
frjaraur <frjaraur@gmail.com>
2016-08-06 16:13:53 +0200

    Changed almost everithing

--- Dockerfile
+++ Dockerfile
@@ -2,40 +2,41 @@ FROM  ubuntu:trusty
 
 MAINTAINER FRJARAUR "frjaraur@gmail.com"
 
-ENV MSERVER_VERSION "1.9"
-
 RUN locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+ENV GOSU_VERSION 1.9
+RUN apt-get update -qq && \
+ LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install -qq --no-install-recommends software-properties-common \
+ apt-utils openjdk-7-jre wget ca-certificates && rm -rf /var/lib/apt/lists/* \
+ && dpkgArch="$(dpkg --print-architecture | awk -F- '{ print $NF }')" \
+ && wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch" \
+ && wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$dpkgArch.asc" \
+ && export GNUPGHOME="$(mktemp -d)" \
+ && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
+ && gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
+ && rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
+ && chmod +x /usr/local/bin/gosu \
+ && gosu nobody true \
+ && apt-get purge -qq --auto-remove apt-utils \
+ && apt-get -qq autoremove
 
-RUN apt-get update && \
- LC_ALL=en_US.UTF-8 DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common apt-utils openjdk-7-jre wget 
+ENV VERSION 1.10
+ENV MODS false
+ENV USERID 3333
+ENV GROUPID ${USERID}
+ENV BASEDIR /MINECRAFT
 
-ENV MSERVER /SERVER
-ENV TMPDATA /DEFAULTS
+RUN groupadd -r mcserver --gid=${GROUPID} && useradd -r -g mcserver --uid=${USERID} mcserver
 
-RUN groupadd -r mserver --gid=2002 && useradd -r -g mserver --uid=2002 mserver
+WORKDIR /MINECRAFT
 
-RUN gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
-RUN wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture)" \
-        && wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture).asc" \
-        && gpg --verify /usr/local/bin/gosu.asc \
-        && rm /usr/local/bin/gosu.asc \
-        && chmod +x /usr/local/bin/gosu
+VOLUME /MINECRAFT
 
-WORKDIR $TMPDATA
+COPY forge forge
 
-RUN wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/${MSERVER_VERSION}/minecraft_server.${MSERVER_VERSION}.jar
+COPY mcserver.sh mcserver.sh
 
-VOLUME "${MSERVER}"
+ENTRYPOINT ["/MINECRAFT/mcserver.sh"]
 
-COPY DEFAULTS/server.properties ${TMPDATA}/server.properties
-COPY DEFAULTS/server-icon.png ${TMPDATA}/server-icon.png
+EXPOSE 25565
 
-COPY docker-entrypoint.sh /docker-entrypoint.sh
-
-ENTRYPOINT ["/docker-entrypoint.sh"]
-
-#If you want to user this server on internet dont forget to change your routing/portnat infastructure.
-#You can use -p INTERNAL_PORT:HOST_PORT when running this container when changing server.properties.
-EXPOSE 25565:25565
-
-CMD ["start","",""]
+CMD ["help"]
---
+++ mcserver.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+ACTION="$(echo $1|tr '[A-Z]' '[a-z]')"
+
+VERSION=${VERSION:=1.10}
+
+MODS=${MODS:=false}
+
+USERID=${USERID:=3333}
+
+GROUPID=${GROUPID:=USERID}
+
+BASEDIR=${BASEDIR:=/MINECRAFT}
+
+# COLORS
+RED='\033[0;31m' # Red
+BLUE='\033[0;34m' # Blue
+GREEN='\033[0;32m' # Green
+CYAN='\033[0;36m' # Cyan
+YELL='\033[1;33m' # Cyan
+NC='\033[0m' # No Color
+
+Help(){
+	printf "${YELL}Use enviroment variables to change server version and mod mode:${NC}\n"
+  printf "${RED}VERSION${YELL} --> ${RED}-e\"VERSION=1.10\"${YELL} (default)${NC}\n"
+  printf "${RED}MODS${YELL} --> ${RED}-e\"MODS=true\"${YELL} (default MODS=false)${NC}\n"
+  printf "${RED}USERID${YELL} --> ${RED}-e\"USERID=3333\"${YELL} (default)${NC}\n"
+  printf "${RED}GROUPID${YELL} --> ${RED}-e\"GROUPID=3333\"${YELL} (default)${NC}\n"
+  printf "${RED}BASEDIR${YELL} Should be a volumen mapped on your host engine.${NC}\n"
+  printf "\n${RED}*Use your userid and groupid if you want to take control of configuration files.${NC}\n"  
+
+}
+
+ErrorMessage(){
+	date="$(date +%Y/%m/%d-%H:%M:%S)"
+	printf "${date} ${RED}ERROR: $* ${NC}\n"
+	exit 1
+}
+
+InfoMessage(){
+	date="$(date +%Y/%m/%d-%H:%M:%S)"
+	printf "${date} ${CYAN}INFO: $* ${NC}\n"
+}
+
+OKMessage(){
+	date="$(date +%Y/%m/%d-%H:%M:%S)"
+	printf "${date} ${GREEN}OK: $* ${NC}\n"
+}
+
+Download(){
+  wget -q -O minecraft_server.${VERSION}.jar https://s3.amazonaws.com/Minecraft.Download/versions/${VERSION}/minecraft_server.${VERSION}.jar
+  [ $? -ne 0 ] && rm -f minecraft_server.${VERSION}.jar && ErrorMessage "Can not download 'minecraft_server.${VERSION}.jar'."
+  OKMessage "Minecraft server 'minecraft_server.${VERSION}.jar' downloaded correctly."
+}
+
+CheckMods(){
+
+  [ "${MODS}" = "false" ] && InfoMessage "Not using any mod on server" && return 0
+
+  # forge-VERSION-
+  [ ! -d forge ] && ErrorMessage "Forge directory does not exists."
+
+  FORGE_VERSION="$(ls -1 forge/forge-${VERSION}-*.jar 2>/dev/null|tail -1)"
+
+  [ ! -n "${FORGE_VERSION}" ] && ErrorMessage "Can not find a valid forge version for your 'minecraft_server.${VERSION}.jar'."
+
+  # sed -e "s/ORIGEN/DESTINO/"
+  FORGE_VERSION="$(echo ${FORGE_VERSION}|sed -e "s/forge\///g")"
+
+  OKMessage "We will use '${FORGE_VERSION}' for 'minecraft_server.${VERSION}.jar'."
+
+}
+
+SetUpEnvironment(){
+
+  MCSERVERDIR="${BASEDIR}/MCSERVER_${VERSION}"
+
+  [ ! -f ${MCSERVERDIR} ] && mkdir -p ${MCSERVERDIR} || ErrorMessage "Can not create server game dir '${MCSERVERDIR}'."
+  if [ "${MODS}" = "false" ]
+  then
+    [ ! -f ${MCSERVERDIR}/minecraft_server.${VERSION}.jar ] \
+    && cp minecraft_server.${VERSION}.jar ${MCSERVERDIR} \
+    || ErrorMessage "Can not copy 'minecraft_server.${VERSION}.jar' to server game dir '${MCSERVERDIR}'."
+  else
+    [ ! -f ${MCSERVERDIR}/${FORGE_VERSION} ] && \
+    cp forge/${FORGE_VERSION} ${MCSERVERDIR} \
+    || ErrorMessage "Can not copy 'forge/${FORGE_VERSION}' to server game dir '${MCSERVERDIR}'."
+  fi
+
+  # Accept eula
+  echo "eula=true" > ${MCSERVERDIR}/eula.txt || ErrorMessage "Can not accept eula on '${MCSERVERDIR}'."
+
+  # Install forge if it is not already installed
+  FORGE_EXEC="$(echo ${FORGE_VERSION}|sed -e "s/-installer./-universal./g")"
+  [ ! -f ${FORGE_EXEC} ] && cd ${MCSERVERDIR} && InfoMessage "Forge not found .... it will be installed" \
+  && java -jar ${FORGE_VERSION} --installServer nogui
+
+  chown -R ${USERID}:${GROUPID} ${MCSERVERDIR}
+}
+
+BackupWorld(){
+
+  [ ! -d ${MCSERVERDIR}/world ] && InfoMessage "Does not exist any previous world, stating from scratch, no backup needed." \
+  && return 0
+
+  # Move previous backup
+  [ -f ${MCSERVERDIR}/world.current.tar.bz ] && \
+  mv ${MCSERVERDIR}/world.current.tar.bz ${MCSERVERDIR}/world.$(date -r ${MCSERVERDIR}/world.current.tar.bz +%Y%m%d%H%M).tar.bz
+
+  # Purge old backups (max 5)
+
+
+  # Create new backup
+  [ -d ${MCSERVERDIR}/world ] && tar -jcf ${MCSERVERDIR}/world.current.tar.bz ${MCSERVERDIR}/world \
+  || ErrorMessage "Can not create '${MCSERVERDIR}/world.current.tar.bz'."
+
+}
+
+
+
+
+
+case ${ACTION} in
+  start)
+    InfoMessage "VERSION=${VERSION}"
+    InfoMessage "MODS=${MODS}"
+    InfoMessage "USERID=${USERID}"
+    InfoMessage "BASEDIR=${BASEDIR}"
+
+    [ ! -f "minecraft_server.${VERSION}.jar" ] && Download
+
+    CheckMods
+
+    SetUpEnvironment
+
+    BackupWorld
+
+    cd ${MCSERVERDIR}
+
+    # Stand Alone or Modded server
+    if [ "${MODS}" = "false" ]
+    then
+      gosu ${USERID} java -Dfml.queryResult=confirm -Xms2048M -Xmx2048M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing \
+      -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar minecraft_server.${VERSION}.jar nogui
+
+      [ $? -ne 0 ] && ErrorMessage "Can not execute correctly 'minecraft_server.${VERSION}.jar' :| or have you used Ctr+C to exit server :O ??."
+
+    else
+
+      gosu ${USERID} java -Dfml.queryResult=confirm -Xms2048M -Xmx2048M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing \
+      -XX:ParallelGCThreads=2 -XX:+AggressiveOpts -jar ${FORGE_EXEC} nogui
+
+      [ $? -ne 0 ] && ErrorMessage "Can not execute correctly '${FORGE_EXEC}' :| ."
+
+    fi
+  ;;
+
+  stop)
+    pkill java || ErrorMessage "Can not stop minecraft server :| ."
+    OKMessage "Minecraft server terminated...."
+
+  ;;
+
+  help)
+    Help
+
+  ;;
+
+  *)
+    OKMessage "EXECUTING \"$@\""
+    exec "$@"
+
+  ;;
+esac
